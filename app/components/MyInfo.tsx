4f17d0bf96b7246949c691e23521964161cd2bf8
Gurdit Singh <105954455+GurditLubana@users.noreply.github.com>
2024-05-12 19:23:46 -0500

    Designed Landing page

--- app/components/MyInfo.tsx
+++ app/components/MyInfo.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+//Add a typewriter effect
 
 function MyInfo() {
   return (
-    <div className='grid grid-cols-1 h-full w-full'>
-      
-      <div className='flex flex-col items-start justify-center p-5 m-auto rounded-lg max-w-2xl'>
-        <h1 className="text-6xl font-bold mb-2">Gurdit Singh</h1>
-        <p className="text-xl font-semibold ">Full Stack Developer</p>
-        <p className="mt-8">
-          In the world of code, I find my purpose; developing impactful software with every line I write. Let&apos;s make technology work for us.
+    <div className="grid grid-cols-1 h-full w-full">
+      <div className="flex flex-col items-start justify-center p-5 m-auto rounded-lg max-w-2xl">
+        <h1 className="text-8xl text-amber-300 font-bold mb-2">Gurdit Singh</h1>
+        <p className="text-xl text-amber-200 font-semibold ">
+          Full Stack Developer
+        </p>
+        <p className="text-2xl mt-8">
+          In the world of code, I find my purpose, developing impactful software
+          with every line I write. Let&apos;s make technology work for us.
         </p>
         <div className="mt-6">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded transition duration-300 ease-in-out transform hover:-translate-y-1">
-            Resume
-          </button>
+          <Link
+            className="py-2 hover:opacity-75 transition-opacity duration-300 me-6 px-6 rounded-sm border-solid border-2 border-amber-200 flex items-center justify-center"
+            href="#"
+          >
+            <p className="text-amber-300">Resume</p>
+          </Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default MyInfo
+export default MyInfo;
