327c176c80d4087bfea08b7ae598ebb79d566439
Gurdit Singh <105954455+GurditLubana@users.noreply.github.com>
2024-05-15 19:47:10 -0500

    Fixed the alignment of the inner circles of the timeline UI

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -7,32 +7,26 @@ function Experience() {
     <div className="h-screen mx-8 px-8">
       <h1 className="text-4xl">Where have I worked: </h1>
       <div className="space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
-        <ExperienceCard 
+        <ExperienceCard
           companyName="Wawanesa Insurance Group"
           role="Administrative Assistant"
           date="February 2023 - Present"
-          
-          
         />
         <ExperienceCard
           companyName="Manitoba Government"
           role="Data Analyst Intern"
           date="April 2023 - September 2023"
-          
         />
         <ExperienceCard
           companyName="University of Manitoba"
           role="Undergraduate Research Assistant in Computational Mathematics"
           date="January 2022 - September 2022"
-          
         />
         <ExperienceCard
           companyName="Air Canada"
           role="Security Officer"
           date="January 2021 - January 2022"
-          
         />
-        
       </div>
     </div>
   );
--- app/components/ExperienceCard.tsx
+++ app/components/ExperienceCard.tsx
@@ -5,10 +5,10 @@ interface ExperienceCardProps {
     companyName: string;
     role: string;
     date: string;
+    
 }
 
-function ExperienceCard({ companyName, role, date }: ExperienceCardProps) {
-    // Animation settings
+function ExperienceCard({ companyName, role, date}: ExperienceCardProps) {
     const cardVariants = {
         hidden: { opacity: 0, y: -50 },
         visible: { opacity: 1, y: 0 }
@@ -16,17 +16,16 @@ function ExperienceCard({ companyName, role, date }: ExperienceCardProps) {
 
     return (
         <motion.div
-            className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active left"
+            className= {`relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active`}
             variants={cardVariants}
             initial="hidden"
             whileInView="visible"
-            viewport={{ once: false, amount: 0.5 }}  // Set once to false
+            viewport={{ once: false, amount: 0.5 }}  
             transition={{ duration: 0.8, delay: 0.2 }}
         >
-            <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 text-slate-500 shadow shrink-0 md:order-1">
-                {/* SVG Icon Here */}
+            <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 text-slate-500 shadow shrink-0 md:order-1 ">
             </div>
-            <div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
+            <div className="w-[calc(100%-4rem)] md:w-[calc(50%-1.25rem)] bg-white p-4 rounded border border-slate-200 shadow">
                 <div className="flex items-center justify-between space-x-2 mb-1">
                     <div className="font-bold text-slate-900">{companyName}</div>
                     <time className="font-caveat font-medium text-indigo-500">{date}</time>
