a9b210163c1e98b330962f08d888150ce9c20128
Gurdit Singh <105954455+GurditLubana@users.noreply.github.com>
2024-06-15 20:35:54 -0500

    Added responsibilities button in experience card

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -1,46 +1,81 @@
 import React from "react";
 import ExperienceCard from "./ExperienceCard";
 
+const experiences = [
+  {
+    companyName: "Wawanesa Insurance Group",
+    role: "Administrative Assistant",
+    date: "February 2023 - Present",
+    logoUrl: "/images/wawanesa.png",
+    responsibilities: [
+      "Managed office supplies and inventory",
+      "Coordinated and scheduled meetings",
+      "Prepared reports and maintained records",
+    ],
+  },
+  {
+    companyName: "Manitoba Government",
+    role: "Programmer Analyst Intern",
+    date: "April 2023 - September 2023",
+    logoUrl: "/images/manitoba-gov.png",
+    responsibilities: [
+      "Developed and maintained web applications",
+      "Collaborated with senior developers on various projects",
+      "Provided technical support and troubleshooting",
+    ],
+  },
+  {
+    companyName: "University of Winnipeg",
+    role: "Security Guard",
+    date: "September 2022 - January 2023",
+    logoUrl: "/images/uofwLogo.png",
+    responsibilities: [
+      "Ensured the safety and security of campus facilities",
+      "Monitored surveillance systems",
+      "Responded to emergency situations",
+    ],
+  },
+  {
+    companyName: "University of Manitoba",
+    role: "Undergraduate Research Assistant in Computational Mathematics",
+    date: "January 2022 - September 2022",
+    logoUrl: "/images/uofm-logo.png",
+    responsibilities: [
+      "Assisted in research projects in computational mathematics",
+      "Collected and analyzed data",
+      "Prepared research reports and presentations",
+    ],
+  },
+  {
+    companyName: "Air Canada",
+    role: "Security Officer",
+    date: "January 2021 - January 2022",
+    logoUrl: "/images/aircanda.png",
+    responsibilities: [
+      "Ensured the safety of passengers and staff",
+      "Conducted security checks and screenings",
+      "Handled emergency situations and incidents",
+    ],
+  },
+];
+
 function Experience() {
   return (
-    <div id="experienceSection" className=" mx-1 md:mx-8 md:px-8 flex flex-col gap-1">
-      <h1 className="text-5xl md:text-6xl font-bold mt-20 my-9 text-amber-300 text-center">Work Experience </h1>
-      <div className=" relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
-        <ExperienceCard
-          companyName="Wawanesa Insurance Group"
-          role="Administrative Assistant"
-          date="February 2023 - Present"
-          logoUrl="/images/wawanesa.png"
-          responsibilities="Details about the responsibilities for this role..."
-        />
-        <ExperienceCard
-          companyName="Manitoba Government"
-          role="Programmer Analyst Intern"
-          date="April 2023 - September 2023"
-          logoUrl="/images/manitoba-gov.png"
-          responsibilities="Details about the responsibilities for this role..."
-        />
-        <ExperienceCard
-          companyName="University of Winnipeg"
-          role="Security Guard"
-          date="September 2022 - January 2023"
-          logoUrl="/images/uofwLogo.png"
-          responsibilities="Details about the responsibilities for this role..."
-        />
-        <ExperienceCard
-        companyName="University of Manitoba"
-        role="Undergraduate Research Assistant in Computational Mathematics"
-        date="January 2022 - September 2022"
-        logoUrl="/images/uofm-logo.png"
-        responsibilities="Details about the responsibilities for this role..."
-      />
-        <ExperienceCard
-          companyName="Air Canada"
-          role="Security Officer"
-          date="January 2021 - January 2022"
-          logoUrl="/images/aircanda.png"
-          responsibilities="Details about the responsibilities for this role..."
-        />
+    <div id="experienceSection" className="mx-1 md:mx-8 md:px-8 flex flex-col gap-1">
+      <h1 className="text-5xl md:text-6xl font-bold mt-20 my-9 text-amber-300 text-center">
+        Work Experience
+      </h1>
+      <div className="relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
+        {experiences.map((experience, index) => (
+          <ExperienceCard
+            key={index}
+            companyName={experience.companyName}
+            role={experience.role}
+            date={experience.date}
+            logoUrl={experience.logoUrl}
+            responsibilities={experience.responsibilities}
+          />
+        ))}
       </div>
     </div>
   );
--- app/components/ExperienceCard.tsx
+++ app/components/ExperienceCard.tsx
@@ -8,7 +8,7 @@ interface ExperienceCardProps {
   role: string;
   date: string;
   logoUrl: string;
-  responsibilities: string; 
+  responsibilities: string[]; 
 }
 
 function ExperienceCard({
@@ -16,14 +16,14 @@ function ExperienceCard({
   role,
   date,
   logoUrl,
-  responsibilities, 
+  responsibilities,
 }: ExperienceCardProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const cardVariants = {
     hidden: { opacity: 0, y: -50 },
     visible: { opacity: 1, y: 0 },
   };
-
 
   return (
     <>
@@ -36,9 +36,9 @@ function ExperienceCard({
         transition={{ duration: 1.5, delay: 0 }}
       >
         <div className="flex items-center justify-center w-14 h-14 mx-8 shrink-0 md:order-1 rounded-full bg-white border-amber-300">
-          <Image 
-            src={logoUrl} 
-            alt={`${companyName} logo`} 
+          <Image
+            src={logoUrl}
+            alt={`${companyName} logo`}
             width={48}
             height={48}
             className="object-contain rounded-full"
@@ -51,11 +51,36 @@ function ExperienceCard({
               <time className="font-caveat font-medium text-amber-100">{date}</time>
             </div>
             <div className="text-center md:text-start text-gray-300">{role}</div>
+            <button
+              onClick={() => setIsModalOpen(true)}
+              className="button text-center text-gray-300 mt-2 md:mx-80"
+            >
+              Responsibilities
+            </button>
           </div>
-          
         </div>
       </motion.div>
 
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white darkBg p-6 rounded-lg w-11/12 md:w-2/3 lg:w-1/2 relative">
+            <button
+              onClick={() => setIsModalOpen(false)}
+              className="absolute top-2 right-2 text-gray-400 hover:text-gray-600"
+            >
+              ✖
+            </button>
+            <h2 className="text-2xl font-bold text-amber-300 mb-4">{companyName}</h2>
+            <h3 className="text-xl font-semibold text-gray-300 mb-4">{role}</h3>
+            <h4 className="text-lg font-medium text-amber-300 mb-4">Responsibilities:</h4>
+            <ul className="list-disc list-inside text-gray-300">
+              {responsibilities.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </>
   );
 }
